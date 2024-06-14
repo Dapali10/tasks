c37c746755c2555cd2bd4574eb879dca627f1c0e
Dapali10 <orlandinidaniel017@gmail.com>
2024-06-14 08:11:42 -0300

    Atualização da tasklist

--- src/screens/Task_list.js
+++ src/screens/Task_list.js
@@ -5,10 +5,14 @@ import today_Image from '../../assets/imgs/today.jpg'
 
 export default class Task_list extends Component{
     render(){
+        const today = moment().locale('pt-br').format('dddd, DD [de] MMMM [de] YYYY')
         retur(
             <View style={styles.conteiner}>
                 <ImageBackground source={today_Image} style={styles.background}>
-
+                    <View style={styles.titleBAR}>
+                        <Text style={styles.title}>Hoje</Text>
+                        <Text style={styles.subTitle}>{today}</Text>
+                    </View>
                 </ImageBackground>
                 <View style={styles.taskList }>
                     <Text>Lista de Tarefas</Text>
@@ -27,5 +31,23 @@ const styles = StyleSheet.create({
     },
     taskList:{
         flex: 7
+    },
+    titleBAR:{
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+    title:{
+        fontFamily: 'Arial',
+        fontSize: 50,
+        color: '#FFF',
+        marginLeft: 20,
+        marginBottom: 20
+    },
+    subTitle:{
+        fontFamily: 'Arial',
+        fontSize: 20,
+        color: '#FFF',
+        marginLeft: 20,
+        marginBottom: 30
     }
 })
